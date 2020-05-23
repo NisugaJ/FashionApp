9cac99ffada77c4f1881071933d3bc2515e2ad4d
Nisuga Jayawardana <44756803+NisugaJ@users.noreply.github.com>
2020-05-24 01:01:06 +0530

    added user table, refined admin side menu

---
+++ client/src/main_components/admin/manager/managersTab.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import ManagersIndex from "./viewManager"
+import CreateManager from "./addmanager"
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+export default function ManagersTabPanel() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+                    <Tab styles={{ textDecoration: "none" }} label="All Managers" {...a11yProps(1)} />
+                    <Tab styles={{ textDecoration: "none" }} label="Add Store Manager" {...a11yProps(0)} />
+                </Tabs>
+            </AppBar>
+            <TabPanel value={value} index={0}>
+                <ManagersIndex />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <CreateManager />
+            </TabPanel>
+
+        </div>
+    );
+}
